1d0ae0709eacd60d6d72ec4bb54fa26edc6569f9
bobo826 <2696468332@qq.com>
2018-11-23 18:54:51 +0800

    工具

--- src/page/home/Home.vue
+++ src/page/home/Home.vue
@@ -1,11 +1,36 @@
 <template>
-  <div class="hello">
+  <div class="tools">
     <h1>{{ msg }}</h1>
     <ul>
       <li v-for="(item,index) in items" :key="index" :class="item.active">{{ item.name }}</li>
     </ul>
     <button type="button" @click="start">开始</button>
     <button type="button" @click="end">停止</button>
+    <h1>{{ jishi }}</h1>
+    <div class="jishi" v-drag>
+      <div class="nav">
+        <span>最小化</span>
+        <span>重置</span>
+        <span>最大化</span>
+      </div>
+      <div class="time">
+        <div class="top">
+          <span>{{ qian }}</span>
+          <span>{{ bai }}</span>
+          <b>:</b>
+          <span>{{ shi }}</span>
+          <span>{{ ge }}</span>
+        </div>
+        <div class="bottom">
+          <span>MINUTES</span>
+          <span>SECONDS</span>
+        </div>
+      </div>
+      <div class="btn">
+        <button type="button" @click="startTime">开始</button>
+        <button type="button" @click="reStart">重置</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -34,11 +59,14 @@ export default {
         {name: '15', active: ''},
         {name: '16', active: ''}
       ],
-      timer: null
+      timer: null,
+      jishi: '计时器',
+      ge: 0, // 个位到十位-10进制
+      shi: 0, // 十位到百位-6进制
+      bai: 0, // 百位千位-10进制
+      qian: 0, // 千位-10进制
+      jishitimer: null
     }
-  },
-  components:{
-    EvilIcons
   },
   methods: {
     start () {
@@ -58,6 +86,54 @@ export default {
     },
     end () {
       clearInterval(this.timer)
+    },
+    startTime () {
+      clearInterval(this.jishitimer)
+      this.jishitimer = setInterval(() => {
+        this.ge++
+        if (this.ge === 9) {
+          this.ge = 0
+          this.shi++
+          if (this.shi === 6) {
+            this.shi = 0
+            this.bai++
+            if (this.bai === 9) {
+              this.bai = 0
+              this.qian++
+            }
+          }
+        }
+      }, 1000)
+    },
+    reStart () {
+      this.ge = 0
+      this.shi = 0
+      this.bai = 0
+      this.qian = 0
+      clearInterval(this.jishitimer)
+    }
+  },
+  directives: {
+    drag: {
+      bind: function (el) {
+        let odiv = el // 获取当前元素
+        odiv.onmousedown = (e) => {
+          // 算出鼠标相对元素的位置
+          let disX = e.clientX
+          let disY = e.clientY
+          document.onmousemove = (e) => {
+            // 用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
+            let left = e.clientX - disX
+            let top = e.clientY - disY
+            odiv.style.left = left + 'px'
+            odiv.style.top = top + 'px'
+          }
+          document.onmouseup = (e) => {
+            document.onmousemove = null
+            document.onmouseup = null
+          }
+        }
+      }
     }
   },
   mounted () {
@@ -70,26 +146,74 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-  padding: 20px;
-  background: grey;
-  text-align: center;
-  color: white;
-}
-.active {
-  background: #42b983;
-}
-.cui-topBar2 {
-  display: none;
-}
+<style lang="scss" scoped>
+  .tools {
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+  }
+  h1, h2 {
+    font-weight: normal;
+  }
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  li {
+    display: inline-block;
+    margin: 0 10px;
+    padding: 20px;
+    background: grey;
+    text-align: center;
+    color: white;
+  }
+  .active {
+    background: #42b983;
+  }
+  .cui-topBar2 {
+    display: none;
+  }
+  .jishi {
+    border-radius: 8px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: auto;
+    flex-direction: column;
+    background: #ffffff;
+    width: 674px;
+    height: 388px;
+    position: relative;
+    top: 10px;
+    left: 10px;
+    .time {
+      background: #5783E5;
+      width: 452px;
+      height: 204px;
+      line-height: 204px;
+      color: white;
+      border-radius: 5px;
+      flex-direction: column;
+    }
+    .top {
+      span {
+        width: 112px;
+        height: 167px;
+        font-size: 48px;
+        background: #ffffff;
+        color: #5783E5;
+      }
+      b {
+        color: #ffffff;
+        font-size: 48px;
+      }
+    }
+    .btn {
+      flex-direction: row;
+      margin: 54px 0 0 0;
+      button {
+        margin: 0 10px 0 10px;
+      }
+    }
+  }
 </style>
